dc2f1d36457c0c5a102166117d4fcf121b70a053
notrixneo <nabil.kugelstadt@gmail.com>
2026-09-02 12:46:50 +0200

    Revision of Yesterdays Excercise and learning about ZerodivisionError and Recursion

---
+++ Run Distance (def function revision).py
@@ -0,0 +1,27 @@
+def analyze_run(distance):
+    if len(distance) == 0:
+            return 0, 0, 0
+    
+    total = sum(distance)
+    average = total / len(distance)
+    longest = max(distance)
+    return total, average, longest
+    
+
+monday_run = [5.2, 3.4]
+tuesday_run = []
+wednesday_run = [6.8]
+thursday_run = [4.1, 2.5, 5.0]
+friday_run = [8.3, 3.0]
+
+mon_total, mon_average, mon_long = analyze_run(monday_run)
+tue_total, tue_average, tue_long = analyze_run(tuesday_run)
+wed_total, wed_average, wed_long = analyze_run(wednesday_run)
+thur_total, thur_average, thur_long = analyze_run(thursday_run)
+fri_total, fri_average, fri_long = analyze_run(friday_run)
+
+print(f"Total Run Distance Monday: {mon_total:.2f} Average {mon_average:.2f} Longest run: {mon_long:.2f}")
+print(f"Total Run Distance Tuesday: {tue_total:.2f} Average {tue_average:.2f} Longest run: {tue_long:.2f}")
+print(f"Total Run Distance Wednesday: {wed_total:.2f} Average {wed_average:.2f} Longest run: {wed_long:.2f}")
+print(f"Total Run Distance Thursday: {thur_total:.2f} Average {thur_average:.2f} Longest run: {thur_long:.2f}")
+print(f"Total Run Distance Friday: {fri_total:.2f} Average {fri_average:.2f} Longest run: {fri_long:.2f}")
